db4873d7607f10879f5485579a0ffb16132fe3e4
vsbuidev <vsb.uidev@gmail.com>
2024-03-15 11:23:36 +0530

    Final correction

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   FlatList,
 } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 const TodoListApp = () => {
   const [tasks, setTasks] = useState([]);
@@ -40,69 +41,78 @@ const TodoListApp = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Daily Todo's</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Add a new task"
-          placeholderTextColor="#BABABA"
-          value={newTask}
-          onChangeText={(text) => setNewTask(text)}
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Daily Todo's</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add a new task"
+            placeholderTextColor="#5C5C5C"
+            value={newTask}
+            onChangeText={(text) => setNewTask(text)}
+          />
+          <TouchableOpacity style={styles.addButton} onPress={addTask}>
+            <Text color="#202020" style={styles.buttonText}>
+              Add Task
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={tasks}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <View
+              style={[styles.taskItem, item.completed && styles.completedTask]}
+            >
+              <TextInput
+                style={[
+                  styles.taskText,
+                  item.completed && styles.completedText,
+                ]}
+                value={item.text}
+                onChangeText={(text) => editTask(index, text)}
+                editable={!item.completed}
+              />
+              <TouchableOpacity
+                style={styles.checkButton}
+                onPress={() => toggleCompleted(index)}
+              >
+                <Text color="#202020" style={styles.buttonText}>
+                  {item.completed ? "Undone" : "Done"}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.removeButton}
+                onPress={() => removeTask(index)}
+              >
+                <Text color="#202020" style={styles.buttonText}>
+                  Remove
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
         />
-        <TouchableOpacity style={styles.addButton} onPress={addTask}>
-          <Text color="#202020" style={styles.buttonText}>
-            Add Task
-          </Text>
-        </TouchableOpacity>
+        <Text style={styles.footerText}>vsb.uidev - © 2024</Text>
       </View>
-      <FlatList
-        data={tasks}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <View
-            style={[styles.taskItem, item.completed && styles.completedTask]}
-          >
-            <TextInput
-              style={[styles.taskText, item.completed && styles.completedText]}
-              value={item.text}
-              onChangeText={(text) => editTask(index, text)}
-              editable={!item.completed}
-            />
-            <TouchableOpacity
-              style={styles.checkButton}
-              onPress={() => toggleCompleted(index)}
-            >
-              <Text color="#202020" style={styles.buttonText}>
-                {item.completed ? "✓" : "Done"}
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.removeButton}
-              onPress={() => removeTask(index)}
-            >
-              <Text color="#202020" style={styles.buttonText}>
-                Remove
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      />
-      <Text style={styles.footerText}>vsb.uidev - © 2024</Text>
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingVertical: 16,
     flex: 1,
-    padding: 24,
     backgroundColor: "#202020",
   },
-  title: {
+  headerText: {
     fontSize: 32,
-    fontWeight: "semibold",
+    fontWeight: "bold",
     padding: 16,
+    paddingBottom: 24,
     textAlign: "center",
     color: "#BABABA",
   },
@@ -110,32 +120,37 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "center",
     color: "#BABABA",
-    padding: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
   },
   inputContainer: {
     flexDirection: "row",
-    marginBottom: 16,
-    padding: 8,
+    marginBottom: 24,
+    paddingBottom: 24,
   },
   input: {
     flex: 1,
+    fontSize: 20,
+    fontWeight: "light",
     height: 56,
     borderColor: "#373737",
     color: "#BABABA",
     borderWidth: 1,
     borderRadius: 8,
-    paddingHorizontal: 16,
+    paddingHorizontal: 24,
+    paddingVertical: 16,
   },
   addButton: {
     marginLeft: 10,
     backgroundColor: "#626ED9",
-    padding: 8,
+    padding: 16,
     borderRadius: 8,
     alignItems: "center",
     justifyContent: "center",
   },
   buttonText: {
     color: "#202020",
+    fontSize: 16,
   },
   taskItem: {
     flexDirection: "row",
